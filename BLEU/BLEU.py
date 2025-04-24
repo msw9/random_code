9ba7965ca102935b9f8a043f9cb38a6cbcc896b2
msw9 <103287262+msw9@users.noreply.github.com>
2025-04-24 11:43:18 +0200

    Add files via upload

---
+++ BLEU/BLEU.py
@@ -0,0 +1,22 @@
+import nltk
+
+with open("mes_resultats.txt", encoding="utf-8") as f:
+    candidates = f.readlines()
+with open("corrige.txt", encoding="utf-8") as f:
+    references = f.readlines()
+
+candidates = [line.strip() for line in candidates]
+references = [line.strip() for line in references]
+
+# 
+if len(references) != len(candidates):
+    raise ValueError("Le nombre de références et de candidats doit être le même.")
+
+# Calcul du score BLEU
+for ref, cand in zip(references, candidates):
+    # Les références doivent être une liste de listes
+    ref_list = [ref.split()]
+    cand_list = cand.split()
+    bleu_score = nltk.translate.bleu_score.sentence_bleu(ref_list, cand_list)
+    with open ("notes.txt",mode="a+", encoding="utf-8") as f:
+        f.write(f"{str(bleu_score)}\n")
