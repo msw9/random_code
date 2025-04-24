e34b6a1d472f8ca2839c6e8b15bffccac9ba3960
msw9 <103287262+msw9@users.noreply.github.com>
2025-04-24 13:49:03 +0200

    Adding Smoothing Function
    NEED TO FIX

--- BLEU/BLEU.py
+++ BLEU/BLEU.py
@@ -1,5 +1,8 @@
 import nltk
-
+"""
+documentation pour SmoothingFunction()
+https://www.nltk.org/api/nltk.translate.bleu_score.html#nltk.translate.bleu_score.SmoothingFunction
+"""
 with open("mes_resultats.txt", encoding="utf-8") as f:
     candidates = f.readlines()
 with open("corrige.txt", encoding="utf-8") as f:
@@ -17,6 +20,6 @@ for ref, cand in zip(references, candidates):
     # Les références doivent être une liste de listes
     ref_list = [ref.split()]
     cand_list = cand.split()
-    bleu_score = nltk.translate.bleu_score.sentence_bleu(ref_list, cand_list)
+    bleu_score = nltk.translate.bleu_score.sentence_bleu(ref_list, cand_list,smoothing_function=SmoothingFunction().method0)
     with open ("notes.txt",mode="a+", encoding="utf-8") as f:
         f.write(f"{str(bleu_score)}\n")
