0fd8509881c2f88d91af43b9c165163b7f797fe0
msw9 <103287262+msw9@users.noreply.github.com>
2025-05-02 18:29:42 +0200

    Add Smoothing Function (fixed)

--- BLEU/BLEU.py
+++ BLEU/BLEU.py
@@ -7,19 +7,23 @@ with open("mes_resultats.txt", encoding="utf-8") as f:
     candidates = f.readlines()
 with open("corrige.txt", encoding="utf-8") as f:
     references = f.readlines()
-
+with open("notes.txt",mode="r+",encoding="utf-8") as f:
+    f.seek(0)
+    f.truncate()
+    
+smooth = nltk.translate.bleu_score.SmoothingFunction()
 candidates = [line.strip() for line in candidates]
 references = [line.strip() for line in references]
 
-# 
+# Error proof
 if len(references) != len(candidates):
     raise ValueError("Le nombre de références et de candidats doit être le même.")
 
-# Calcul du score BLEU
+# Calcul BLEU
 for ref, cand in zip(references, candidates):
     # Les références doivent être une liste de listes
     ref_list = [ref.split()]
     cand_list = cand.split()
-    bleu_score = nltk.translate.bleu_score.sentence_bleu(ref_list, cand_list,smoothing_function=SmoothingFunction().method0)
+    bleu_score = nltk.translate.bleu_score.sentence_bleu(ref_list, cand_list,smoothing_function= smooth.method1)
     with open ("notes.txt",mode="a+", encoding="utf-8") as f:
         f.write(f"{str(bleu_score)}\n")
